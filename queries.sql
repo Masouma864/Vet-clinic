daeecaf41aae927e977ac99f07d0a65511fb2510
masouma <masouma2020@gmail.com>
2022-12-14 17:01:01 +0430

    Use savepoint

--- queries.sql
+++ queries.sql
@@ -46,3 +46,12 @@ DELETE FROM animals;
 SELECT * FROM animals;
 ROLLBACK;
 SELECT * FROM animals;
+
+-- Delete all animals born after Jan 1st, 2022.
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT SP1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SAVEPOINT SP1;
+UPDATE animals SET weight_kg = -weight_kg WHERE weight_kg < 0;
+COMMIT;  -- Commit transaction
