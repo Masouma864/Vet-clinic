2754d75511a09a0c248c7203b50473ad8003ffcb
masouma <masouma2020@gmail.com>
2022-12-14 10:54:25 +0430

    modify the inserted data

--- data.sql
+++ data.sql
@@ -2,7 +2,7 @@ INSERT INTO animals (id, names, date_of_birth, escape_attempts, neutered, weight
 VALUES (1,'Agumon', '2020-02-03', '0' , TRUE, '10.23' );
 
 INSERT INTO animals (id, names, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (2,' Gabumon', '2018-11-15', '3' , TRUE, '8' );
+VALUES (2,' Gabumon', '2018-11-15', '2' , TRUE, '8' );
 
 INSERT INTO animals (id, names, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES (3,' Pikachu', '2021-01-07', '1' , FALSE, '15.04');
