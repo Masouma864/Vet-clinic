6d9a6c573dcfe95a6f43fd5bc7ef6a6ab7fa0a9d
masouma <masouma2020@gmail.com>
2022-12-14 16:43:26 +0430

    add a column to table

--- schema.sql
+++ schema.sql
@@ -1,8 +1,13 @@
+/* Database schema to keep the structure of entire database. */
+
 CREATE TABLE animals (
-    id INT BIGSERIAL NOT NULL PRIMARY KEY,
+    id INT,
     names VARCHAR(100) NOT NULL,
     date_of_birth DATE,
     escape_attempts INTEGER,
     neutered BOOLEAN,
-    weight_kg DECIMAL
+    weight_kg DECIMAL,
+
 );
+-- Create column species
+ALTER TABLE animals ADD species VARCHAR(40);
