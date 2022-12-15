280bf39747643388243523af2f820ef4b590b915
masouma <masouma2020@gmail.com>
2022-12-15 11:12:48 +0430

    Modify the column name

--- queries.sql
+++ queries.sql
@@ -1,24 +1,24 @@
--- Find all animals whose names ends in "mon".
+-- Find all animals whose name ends in "mon".
 
-SELECT * from animals where names LIKE '%mon';
+SELECT * from animals where name LIKE '%mon';
 
--- List the names of all animals born between 2016 and 2019.
-SELECT names from animals where date_of_birth  BETWEEN '2016-01-01' AND '2019-12-31';
+-- List the name of all animals born between 2016 and 2019.
+SELECT name from animals where date_of_birth  BETWEEN '2016-01-01' AND '2019-12-31';
 
--- List the names of all animals that are neutered and have less than 3 escape attempts.
-SELECT names from animals where escape_attempts < 3 AND neutered =true;
+-- List the name of all animals that are neutered and have less than 3 escape attempts.
+SELECT name from animals where escape_attempts < 3 AND neutered =true;
 
 -- List date of birth of all animals named either "Agumon" or "Pikachu".
-SELECT date_of_birth from animals where names='Agumon' OR names='Pikachu';
+SELECT date_of_birth from animals where name='Agumon' OR name='Pikachu';
 
--- List names and escape attempts of animals that weigh more than 10.5kg
-SELECT names,escape_attempts from animals where weight_kg > 10.5;
+-- List name and escape attempts of animals that weigh more than 10.5kg
+SELECT name,escape_attempts from animals where weight_kg > 10.5;
 
 -- Find all animals that are neutered.
 SELECT * from animals where neutered =true;
 
 -- Find all animals not named Gabumon..
-SELECT * from animals where names!='Gabumon.' ;
+SELECT * from animals where name!='Gabumon.' ;
 
 -- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
 SELECT * from animals where weight_kg>=10.4 AND weight_kg<=17.3;
@@ -31,7 +31,7 @@ ROLLBACK;
 -- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
 -- Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
 BEGIN;
-UPDATE animals SET species = 'digimon' WHERE names LIKE '%mon';
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
 SELECT species FROM animals;
 UPDATE animals SET species = 'pokemon' WHERE species IS NULL ;
 SELECT species FROM animals;
